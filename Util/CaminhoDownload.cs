f1456cbfef4611f3d10d5a677882b64859896b6c
GabrielAstra <gvlc2013@gmail.com>
2023-08-13 16:58:53 -0300

    Projeto em construção

---
+++ Util/CaminhoDownload.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Work.Util
+{
+    public class CaminhoDownload
+    {
+        public string VideoUrl { get; set; }
+        public string DownloadDirectory { get; set; }
+    }
+}
